d764a9a1ee12765dafd2ace33113ddf20d8f24ee
Bhim Upadhyaya <bpupadhyaya@gmail.com>
2017-04-09 15:06:40 -0700

    Initial project structure.

---
+++ scala-volume-4-programs/src/main/scala/com/equalinformation/scalavol4/scala/Test2.scala
@@ -0,0 +1,8 @@
+package com.equalinformation.scalavol4.scala
+
+/**
+  * Created by bpupadhyaya on 4/9/17.
+  */
+class Test2 {
+
+}
---
+++ scala-volume-4-programs/src/test/scala/com/equalinformation/scalavol4/scala/test/Test4.scala
@@ -0,0 +1,8 @@
+package com.equalinformation.scalavol4.scala.test
+
+/**
+  * Created by bpupadhyaya on 4/9/17.
+  */
+class Test4 {
+
+}
